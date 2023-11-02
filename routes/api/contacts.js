5641a4407429301762c891700bf57b3af624c87b
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-11-02 16:43:24 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,54 @@
-const express = require('express')
+const express = require("express");
+// const cors = require("cors");
+const { listContacts, getContactById } = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.use(cors());
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res, next) => {
+  listContacts()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => next(err));
+  
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const id = req.url.slice(1, req.url.length);
+  // console.log({ id });
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    getContactById(id)
+      .then((data) => {
+        if (data === undefined) {
+          res.json({ message: "Not found" })
+        }
+      res.json(data);
+    })
+    .catch((err) => next(err));
+  // res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/contacts", async (req, res, next) => {
+  // res.json({ message: "template message" });
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  const id = req.url.slice(1, req.url.length);
+  removeContact(id)
+      .then((data) => {
+        if (data === undefined) {
+          res.json({ message: "Not found" })
+        }
+      res.json({ message: "Contact deleted" });
+    })
+    .catch((err) => next(err));
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
