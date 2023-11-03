8a94db240fe232b0a866b4e30550aa461c83ce23
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-11-03 15:59:44 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -4,60 +4,64 @@ const crypto = require("node:crypto");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
+async function readContacts() {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(data);
+}
+
+function writeContacts(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+}
+
 const listContacts = async () => {
-  const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
-  return JSON.parse(contacts);
+  const contacts = await readContacts();
+  return contacts;
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
-  const contact = JSON.parse(contacts).find((contact) => contact.id === contactId);
-  
-  // if (contact === undefined) {
-  //   return null;
-  // }
+  const contacts = await readContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
 
   return contact;
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  const contacts = await readContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
-  
+
   if (index === -1) {
     return null;
   }
 
   const removedContact = contacts.splice(index, 1);
 
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+  await writeContacts(contacts);
 
   return removedContact;
 };
 
 const addContact = async (body) => {
-  // const { name, email, phone } = body;
-  const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  const contacts = await readContacts();
   const newContact = { id: crypto.randomUUID(), ...body };
 
   contacts.push(newContact);
 
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+  await writeContacts(contacts);
 
   return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  const contacts = await readContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
-  
+
   if (index === -1) {
     return null;
   }
 
-  const updatedContact = contacts.splice(index, 1, {id: contactId, ...body});
+  const updatedContact = contacts.splice(index, 1, { id: contactId, ...body });
 
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+  await writeContacts(contacts);
 
   return updatedContact;
 };
