3e71ae5fe8e405b95f2f77cc82d232f8054809d2
M-Tyshchenko <m.tish2406@gmail.com>
2023-11-17 22:53:32 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const UserController = require("../../controllers/auth");
 
-const {authenticate} = require("../../middlewares")
+const { authenticate } = require("../../middlewares");
 
 const jsonParser = express.json();
 
@@ -13,5 +13,9 @@ router.post("/register", jsonParser, UserController.register);
 router.post("/login", jsonParser, UserController.login);
 
 router.get("/current", authenticate, UserController.current);
+
+router.post("/logout", authenticate, UserController.logout);
+
+router.patch("/", authenticate, jsonParser, UserController.updateStatusUser);
 
 module.exports = router;
