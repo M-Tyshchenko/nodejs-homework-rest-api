41729de6476622ef4a1c454130a00c6f1afb293e
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-11-24 11:08:47 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -20,7 +20,13 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    avatarURL: {
+      type: String,
+      required: true,
+      // default:
+    },
   },
+
   { versionKey: false }
 );
 
